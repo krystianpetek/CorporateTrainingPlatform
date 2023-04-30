4dc3ef2a35300391f38e26cc5a7b02353bb9ddf4
Krystian Petek <krystianpetek2@gmail.com>
2023-04-30 10:31:30 +0200

    Created implementation for events dispatcher to handle all events tasks asynchronously and concurrent

---
+++ src/GarageGenius.Shared.Infrastructure/Events/EventDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using GarageGenius.Shared.Abstractions.Events;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GarageGenius.Shared.Infrastructure.Events;
+internal class EventDispatcher : IEventDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EventDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : IEvent
+    {
+        using IServiceScope? serviceScope = _serviceProvider.CreateScope();
+        IEnumerable<IEventHandler<IEvent>> eventHandlers = serviceScope.ServiceProvider.GetServices<IEventHandler<IEvent>>();
+        
+        IEnumerable<Task> tasks = eventHandlers.Select(eventToHandle => eventToHandle.HandleAsync(@event, cancellationToken));
+        await Task.WhenAll(tasks);
+    }
+}
