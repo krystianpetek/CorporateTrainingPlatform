c59114b9dcaa075a3a7d766d2081aa3b04318ead
Krystian Petek <krystianpetek2@gmail.com>
2023-04-30 10:47:48 +0200

    Created event handler generic registration in DI container and also created registration to shared infrastructure project in main modular monolith application

---
+++ src/GarageGenius.Shared.Infrastructure/Events/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using GarageGenius.Shared.Abstractions.Events;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GarageGenius.Shared.Infrastructure.Events;
+public static class Extensions
+{
+    public static IServiceCollection AddEvents(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        IEnumerable<Type> types = assemblies.SelectMany(x => x.GetTypes().Where(t => t.GetInterfaces().Any(any => any.IsGenericType && any.GetGenericTypeDefinition() == typeof(IEventHandler<>))));
+        foreach(var type in types)
+        {
+            services.AddScoped(type.GetInterfaces().First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEventHandler<>)), type);
+        }
+        return services;
+    }
+}
---
+++ src/GarageGenius.Shared.Infrastructure/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using GarageGenius.Shared.Infrastructure.Events;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GarageGenius.Shared.Infrastructure;
+public static class Extensions
+{
+    public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IList<Assembly> assemblies)
+    {
+        services.AddEvents(assemblies);
+        return services;
+    }
+}
