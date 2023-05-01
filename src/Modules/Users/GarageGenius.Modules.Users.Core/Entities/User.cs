eeaa5a5356ae1e2e3e8b0d1d8179ab3b78ed3824
Krystian Petek <krystianpetek2@gmail.com>
2023-05-01 08:37:45 +0200

    Created mapping conventions between EF Core and domain classes Added dependency to EF design package to ensure creating migration in other ddl's

--- src/Modules/Users/GarageGenius.Modules.Users.Core/Entities/User.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Entities/User.cs
@@ -1,15 +1,15 @@
 ﻿using GarageGenius.Modules.Users.Core.ValueObjects;
 
 namespace GarageGenius.Modules.Users.Core.Entities;
-internal class User
+internal record User
 {
-    public Guid Id { get; private set; }
-    public Guid RoleId { get; private set; }
-    public Email Email { get; private set; }
-    public string Password { get; private set; }
-    public Role Role { get; private set; }
-    public UserState State { get; private set; }
-    public DateTime CreatedDate { get; private set; }
+    public Guid Id { get; set; }
+    public Guid RoleId { get; set; }
+    public Email Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    public UserState State { get; set; }
+    //public DateTime CreatedDate { get; private set; }
 }
 
 
---
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Persistance/Configurations/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using GarageGenius.Modules.Users.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageGenius.Modules.Users.Core.Persistance.Configurations;
+internal class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.Property(t => t.Permissions)
+            .HasConversion(x => string.Join(',', x), x => x.Split(',', StringSplitOptions.None));
+    }
+}
