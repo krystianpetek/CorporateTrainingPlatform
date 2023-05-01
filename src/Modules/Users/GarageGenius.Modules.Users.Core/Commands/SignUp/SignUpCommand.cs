7902440ddad483d79fddc02f858ae81df74320d3
Krystian Petek <krystianpetek2@gmail.com>
2023-05-01 11:24:31 +0200

    Created command for sign up user and command handler for this request ( WIP)

---
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Commands/SignUp/SignUpCommand.cs
@@ -0,0 +1,9 @@
+﻿using GarageGenius.Shared.Abstractions.Commands;
+using System.ComponentModel.DataAnnotations;
+
+namespace GarageGenius.Modules.Users.Core.Commands.SignUp;
+
+public record SignUpCommand([Required][EmailAddress] string Email, [Required] string Password, string Role) : ICommand
+{
+    public Guid UserId { get; init; } = Guid.NewGuid();
+}
---
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Commands/SignUp/SignUpCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using GarageGenius.Shared.Abstractions.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageGenius.Modules.Users.Core.Commands.SignUp;
+internal class SignUpCommandHandler : ICommandHandler<SignUpCommand>
+{
+    public Task HandleAsync(SignUpCommand command, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
