8fe8623516f7b7a210f68882ed551328f9df6795
Krystian Petek <krystianpetek2@gmail.com>
2023-05-01 08:55:06 +0200

    Created users repository to operate on database for users schema

--- src/Modules/Users/GarageGenius.Modules.Users.Core/Persistance/Repository/UserRepository.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Persistance/Repository/UserRepository.cs
@@ -1,6 +1,47 @@
-﻿using GarageGenius.Modules.Users.Core.Repositories;
+﻿using GarageGenius.Modules.Users.Core.Entities;
+using GarageGenius.Modules.Users.Core.Persistance.DbContexts;
+using GarageGenius.Modules.Users.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace GarageGenius.Modules.Users.Core.Persistance.Repository;
 internal class UserRepository : IUserRepository
 {
+    private readonly UsersDbContext _usersDbContext;
+
+    internal UserRepository(UsersDbContext usersDbContext)
+    {
+        _usersDbContext = usersDbContext;
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _usersDbContext.AddAsync(user);
+        await _usersDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        User? user = await _usersDbContext.Users.FindAsync(id);
+        if (user is not null)
+        {
+            user.State = UserState.Unactive;
+            await _usersDbContext.SaveChangesAsync();
+        }
+    }
+
+    public Task<User> GetAsync(Guid id)
+    {
+        return _usersDbContext.Users.Include(x => x.Role).SingleOrDefaultAsync(x => x.Id == id);
+    }
+
+    public Task<User> GetAsync(string email)
+    {
+        return _usersDbContext.Users.Include(x => x.Role).SingleOrDefaultAsync(x => x.Email == email);
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _usersDbContext.Update(user);
+        await _usersDbContext.SaveChangesAsync();
+    }
 }
--- src/Modules/Users/GarageGenius.Modules.Users.Core/Repositories/IUserRepository.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Repositories/IUserRepository.cs
@@ -1,4 +1,11 @@
-﻿namespace GarageGenius.Modules.Users.Core.Repositories;
+﻿using GarageGenius.Modules.Users.Core.Entities;
+
+namespace GarageGenius.Modules.Users.Core.Repositories;
 internal interface IUserRepository
 {
+    Task<User> GetAsync(Guid id);
+    Task<User> GetAsync(string email);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(Guid id);
 }
