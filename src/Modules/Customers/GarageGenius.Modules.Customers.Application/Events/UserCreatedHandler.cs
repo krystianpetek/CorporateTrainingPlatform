b38b8f7776d85ef19a9bb0b94a52881b6a6a503d
Krystian Petek <krystianpetek2@gmail.com>
2023-05-06 13:41:21 +0200

    Added EventHandlers from Users to Customers to ensure change state when event occured in Users module

---
+++ src/Modules/Customers/GarageGenius.Modules.Customers.Application/Events/UserCreatedHandler.cs
@@ -0,0 +1,12 @@
+﻿using GarageGenius.Shared.Abstractions.Events;
+
+namespace GarageGenius.Modules.Users.Core.Events;
+public sealed class UserCreatedHandler : IEventHandler<UserCreated>
+{
+    public UserCreatedHandler()
+    {
+    }
+
+    public async Task HandleAsync(UserCreated @event, CancellationToken cancellationToken = default) =>
+        await Console.Out.WriteLineAsync("WIP but works");
+}
