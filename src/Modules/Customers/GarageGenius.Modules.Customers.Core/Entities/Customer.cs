bffb9c0651f06ba628ba2bd36a1fc425e7bc95ed
Krystian Petek <krystianpetek2@gmail.com>
2023-05-07 17:55:54 +0200

    Changed implementation of domain class Customer, encapsulated class, added base AuditableEntity, created constructors (WIP - maybe business logic)

--- src/Modules/Customers/GarageGenius.Modules.Customers.Core/Entities/Customer.cs
+++ src/Modules/Customers/GarageGenius.Modules.Customers.Core/Entities/Customer.cs
@@ -1,12 +1,24 @@
-﻿using GarageGenius.Modules.Customers.Core.ValueObjects;
+﻿using GarageGenius.Modules.Customers.Core.Types;
+using GarageGenius.Modules.Customers.Core.ValueObjects;
+using GarageGenius.Shared.Abstractions.Common;
 
 namespace GarageGenius.Modules.Customers.Core.Entities;
-internal class Customer
+internal class Customer : AuditableEntity
 {
-    public CustomerId Id { get; set; }
-    public UserId CustomerId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public PhoneNumber PhoneNumber { get; set; }
-    public EmailAddress EmailAddress { get; set; }
+    public CustomerId Id { get; private set; }
+    public UserId UserId { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public PhoneNumber PhoneNumber { get; private set; }
+    public EmailAddress EmailAddress { get; private set; }
+
+    private Customer() { }
+    public Customer(UserId userId, string firstName, string lastName, PhoneNumber phoneNumber, EmailAddress emailAddress)
+    {
+        UserId = userId;
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        EmailAddress = emailAddress; // validation email
+    }
 }
