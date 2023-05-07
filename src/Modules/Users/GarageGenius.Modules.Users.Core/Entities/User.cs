711d8c7d83d4ea18ec88d1e5118bb9b0b57549cc
Krystian Petek <krystianpetek2@gmail.com>
2023-05-07 22:07:22 +0200

    Updated Role column on User entity, created relation to Role entity, updated migrations

--- src/Modules/Users/GarageGenius.Modules.Users.Core/Entities/User.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Entities/User.cs
@@ -5,9 +5,10 @@ namespace GarageGenius.Modules.Users.Core.Entities;
 internal sealed class User : AuditableEntity
 {
     public Guid Id { get; private set; }
+    public string RoleId { get; init; }
+    public Role Role { get; private set; }
     public Email Email { get; private set; }
     public string Password { get; private set; }
-    public Role Role { get; private set; }
     public UserState State { get; private set; }
 
     public User(Email email, string password, Role role, UserState state)
@@ -16,8 +17,11 @@ internal sealed class User : AuditableEntity
         Email = email;
         Password = password;
         Role = role;
+        RoleId = role.Name;
         State = state;
     }
+
+    private User() { }
 
     internal void Deactivate()
     {
---
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Persistance/Migrations/20230507195112_RoleUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageGenius.Modules.Users.Core.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
