d6df67969dac5edf897050c3ef743603fd6d629c
Krystian Petek <krystianpetek2@gmail.com>
2023-05-08 22:05:08 +0200

    Added external event handler after signUp user in customers module

---
+++ src/Modules/Customers/GarageGenius.Modules.Customers.Application/Events/External/UserCreatedHandler.cs
@@ -0,0 +1,20 @@
+﻿using GarageGenius.Modules.Customers.Core.Entities;
+using GarageGenius.Modules.Customers.Core.Repositories;
+using GarageGenius.Modules.Users.Shared.Events;
+using GarageGenius.Shared.Abstractions.Events;
+
+namespace GarageGenius.Modules.Customers.Application.Events.External;
+internal sealed class UserCreatedHandler : IEventHandler<UserCreated>
+{
+    private readonly ICustomerRepository _customerRepository;
+    
+    public UserCreatedHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task HandleAsync(UserCreated @event, CancellationToken cancellationToken = default)
+    {
+        await _customerRepository.AddAsync(new Customer(@event.UserId, @event.Email));
+    }
+}
