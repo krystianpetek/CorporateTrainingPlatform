770b6717bed3dcd55a4340c2dca9b4cc89115697
Krystian Petek <krystianpetek2@gmail.com>
2023-05-10 20:28:22 +0200

    Notifications module - created references between layers, added health check and settings.json, registered module

---
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Api/NotificationsModule.cs
@@ -0,0 +1,22 @@
+﻿using GarageGenius.Modules.Notifications.Core;
+using GarageGenius.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GarageGenius.Modules.Notifications.Api;
+internal class NotificationsModule : IModule
+{
+    public const string BasePath = "notifications-module";
+    public string Name { get; } = "Notifications";
+    public IEnumerable<string> Policies { get; } = new string[] { "notifications" };
+
+    public void Register(IServiceCollection services)
+    {
+        services.AddNotificationsCore();
+    }
+
+    public void Use(IApplicationBuilder app)
+    {
+        app.UseHealthChecks($"/health/{BasePath}");
+    }
+}
---
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Core/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("GarageGenius.Modules.Notifications.Api")]
+namespace GarageGenius.Modules.Notifications.Core;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddNotificationsCore(this IServiceCollection services)
+    {
+        return services;
+    }
+}
