9c9220aa597d59485a90badd6f283331ea16805c
Krystian Petek <krystianpetek2@gmail.com>
2023-05-10 18:04:26 +0200

    Changed implementation of SignUp and SignIn query/command by add default values

--- src/Modules/Users/GarageGenius.Modules.Users.Core/Commands/SignUp/SignUpCommand.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Commands/SignUp/SignUpCommand.cs
@@ -1,6 +1,27 @@
 ﻿using GarageGenius.Shared.Abstractions.Commands;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace GarageGenius.Modules.Users.Core.Commands.SignUp;
 
-public record SignUpCommand([Required][EmailAddress] string Email, [Required] string Password, string Role) : ICommand;
+public record SignUpCommand : ICommand
+{
+    [Required]
+    [EmailAddress]
+    [DefaultValue("krystianpetek2@gmail.com")]
+    public string Email { get; init; }
+
+    [Required]
+    [DefaultValue("Password!23")]
+    public string Password { get; init; }
+
+    [DefaultValue("admin")]
+    public string Role { get; init; }
+
+    public SignUpCommand(string email, string password, string role)
+    {
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+}
--- src/Modules/Users/GarageGenius.Modules.Users.Core/Queries/SignIn/SignInQuery.cs
+++ src/Modules/Users/GarageGenius.Modules.Users.Core/Queries/SignIn/SignInQuery.cs
@@ -1,5 +1,23 @@
 ﻿using GarageGenius.Shared.Abstractions.Queries;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace GarageGenius.Modules.Users.Core.Queries.SignIn;
-public record SignInQuery([Required][EmailAddress] string Email, [Required] string Password) : IQuery<string>;
+
+public record SignInQuery : IQuery<string>
+{
+    [Required]
+    [EmailAddress]
+    [DefaultValue("krystianpetek2@gmail.com")]
+    public string Email { get; init; }
+
+    [Required]
+    [DefaultValue("Password!23")]
+    public string Password { get; init; }
+
+    public SignInQuery(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
