dd5ddb5c2992fc7ab35d5cb413bd26fe51a8fb57
Krystian Petek <krystianpetek2@gmail.com>
2023-05-11 22:14:01 +0200

    Added logger to UserCreatedHandler in customers module, changed project where Serilog is set to more based project

--- src/Modules/Customers/GarageGenius.Modules.Customers.Application/Events/External/UserCreatedHandler.cs
+++ src/Modules/Customers/GarageGenius.Modules.Customers.Application/Events/External/UserCreatedHandler.cs
@@ -6,15 +6,18 @@ using GarageGenius.Shared.Abstractions.Events;
 namespace GarageGenius.Modules.Customers.Application.Events.External;
 internal sealed class UserCreatedHandler : IEventHandler<UserCreated>
 {
+    private readonly Serilog.ILogger _logger;
     private readonly ICustomerRepository _customerRepository;
 
-    public UserCreatedHandler(ICustomerRepository customerRepository)
+    public UserCreatedHandler(Serilog.ILogger logger, ICustomerRepository customerRepository)
     {
+        _logger = logger;
         _customerRepository = customerRepository;
     }
 
     public async Task HandleAsync(UserCreated @event, CancellationToken cancellationToken = default)
     {
         await _customerRepository.AddAsync(new Customer(@event.UserId, @event.Email));
+        _logger.Information($"Handled UserCreated event and added customer with user ID: {@event.UserId} and email: {@event.Email}");
     }
 }
