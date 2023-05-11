cf64f0f37bd6fc9256d0ca01753df83475c4b888
Krystian Petek <krystianpetek2@gmail.com>
2023-05-11 13:41:46 +0200

    Created Car domain entity with database entity configuration

---
+++ src/Modules/Cars/GarageGenius.Modules.Cars.Core/Entities/Car.cs
@@ -0,0 +1,34 @@
+﻿using GarageGenius.Shared.Abstractions.Common;
+
+namespace GarageGenius.Modules.Cars.Core.Entities;
+internal sealed class Car : AuditableEntity
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public string Manufacturer { get; private set; }
+    public string Model { get; private set; }
+    public string? Year { get; private set; }
+    public string? Vin { get; private set; }
+    public string? LicensePlate { get; private set; }
+
+    private Car() { }
+
+    public Car(Guid customerId, string manufacturer, string model)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        Manufacturer = manufacturer;
+        Model = model;
+    }
+
+    public Car(Guid customerId, string manufacturer, string model, string? year, string? vin, string? licensePlate)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        Manufacturer = manufacturer;
+        Model = model;
+        Year = year;
+        Vin = vin;
+        LicensePlate = licensePlate;
+    }
+}
---
+++ src/Modules/Cars/GarageGenius.Modules.Cars.Infrastructure/Persistance/Configurations/CarConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using GarageGenius.Modules.Cars.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GarageGenius.Modules.Cars.Infrastructure.Persistance.Configurations;
+internal class CarConfiguration : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.HasIndex(x => x.Vin).IsUnique();
+        builder.Property(x => x.Vin)
+            .HasMaxLength(15);
+
+        builder.Property(x => x.Model)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Manufacturer)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.LicensePlate)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.CustomerId)
+            .IsRequired();
+    }
+}
