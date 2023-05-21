25c1c50943da17fda7212bc409aaad47153404d6
Krystian Petek <krystianpetek2@gmail.com>
2023-05-21 08:16:20 +0200

    Added config for allow websocket in angular proxy for signalR connection

--- src/WebUI/GarageGenius.WebUI/src/proxy.conf.js
+++ src/WebUI/GarageGenius.WebUI/src/proxy.conf.js
@@ -1,11 +1,20 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/health/",
+      "/health",
     ],
     target: "https://localhost:7283",
-    secure: false
-  }
+    secure: false,
+    ws: false
+  },
+  {
+    context: [
+      "/notifications",
+    ],
+    target: "https://localhost:7283",
+    secure: false,
+    ws: true
+  },
 ]
 
 module.exports = PROXY_CONFIG;
