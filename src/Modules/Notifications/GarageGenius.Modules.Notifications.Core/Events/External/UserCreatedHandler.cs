c228a98fc62292cb9e0e0847ac017bcdc57b0d0f
Krystian Petek <krystianpetek2@gmail.com>
2023-05-23 17:38:58 +0200

    Removed action in Reservation module controller which published event to signalR hub and added handling this operation to Notifications module

--- src/Modules/Notifications/GarageGenius.Modules.Notifications.Core/Events/External/UserCreatedHandler.cs
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Core/Events/External/UserCreatedHandler.cs
@@ -1,24 +1,31 @@
 ﻿using GarageGenius.Modules.Users.Shared.Events;
 using GarageGenius.Shared.Abstractions.Events;
+using GarageGenius.Shared.Infrastructure.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
 namespace GarageGenius.Modules.Notifications.Core.Events.External;
 internal sealed class UserCreatedHandler : IEventHandler<UserCreated>
 {
     private readonly Serilog.ILogger _logger;
+    private readonly IHubContext<NotificationsHub> _hubContextNotifications;
 
-    public UserCreatedHandler(Serilog.ILogger logger)
+    public UserCreatedHandler(
+        Serilog.ILogger logger,
+        IHubContext<NotificationsHub> hubContextNotifications)
     {
         _logger = logger;
+        _hubContextNotifications = hubContextNotifications;
     }
 
-    public Task HandleEventAsync(UserCreated @event, CancellationToken cancellationToken = default)
+    public async Task HandleEventAsync(UserCreated @event, CancellationToken cancellationToken = default)
     {
-        // TODO: Send email notification to user
+        await _hubContextNotifications.Clients.All.SendAsync("SendNotification", DateTime.Now, @event.Email);
+        
         _logger.Information(
             messageTemplate: "Event {EventName} handled by {ModuleName} module, added customer with user ID: {UserId}",
             nameof(UserCreated),
             nameof(Notifications),
             @event.UserId);
-        return Task.CompletedTask;
+        // TODO: Send email notification to user ?
     }
 }
--- src/Modules/Reservations/GarageGenius.Modules.Reservations.Api/Controllers/ReservationsController.cs
+++ src/Modules/Reservations/GarageGenius.Modules.Reservations.Api/Controllers/ReservationsController.cs
@@ -4,28 +4,21 @@ using GarageGenius.Shared.Infrastructure.SignalR.Hubs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace GarageGenius.Modules.Reservations.Api.Controllers;
 public class ReservationsController : BaseController
 {
     private readonly IDispatcher _dispatcher;
-    private readonly IHubContext<NotificationsHub> _hubContextNotifications;
 
-    public ReservationsController(IDispatcher dispatcher, IHubContext<NotificationsHub> hubContextNotifications)
+    public ReservationsController(IDispatcher dispatcher)
     {
         _dispatcher = dispatcher;
-        _hubContextNotifications = hubContextNotifications;
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> Get(string data)
-    {
-        await _hubContextNotifications.Clients.All.SendAsync("SendNotification", DateTime.Now, data);
-        return Ok();
     }
 
     [HttpPost]
     [Authorize]
+    [SwaggerOperation("Add reservation")]
     public async Task<IActionResult> AddReservation(AddReservationCommand command)
     {
         await _dispatcher.DispatchCommandAsync(command);
