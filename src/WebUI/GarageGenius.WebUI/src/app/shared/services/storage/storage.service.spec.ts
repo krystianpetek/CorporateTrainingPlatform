b05319a106695127ea72cfdeeca4aa728a235c3b
Krystian Petek <krystianpetek2@gmail.com>
2023-06-17 05:33:38 +0200

    Created tests for storage service in WebUI

--- src/WebUI/GarageGenius.WebUI/src/app/shared/services/storage/storage.service.spec.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/shared/services/storage/storage.service.spec.ts
@@ -4,16 +4,36 @@ import {
   IStorageService,
   BaseStorageService,
 } from './models/base-storage.service';
+import { StorageService } from './storage.service';
 
-describe('StorageService', () => {
+fdescribe('StorageService', () => {
   let service: IStorageService;
+  const key = 'key';
+  const value = 'value';
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(BaseStorageService);
+    service = TestBed.inject(StorageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('[getKey] should return null value when key in storage is not initialized', () => {
+    expect(service.getKey(key)).toEqual(null);
+  });
+
+  it('[setKey, getKey] should set value and then return this value from storage', () => {
+    service.setKey(key, value);
+    expect(service.getKey(key)).toEqual(value);
+  });
+
+  it('[setKey, deleteKey, getKey] should set value then delete value and return empty value from storage', () => {
+    service.setKey(key, value);
+    expect(service.getKey(key)).toEqual(value);
+
+    service.deleteKey(key);
+    expect(service.getKey(key)).toEqual(null);
   });
 });
