38e259c60add8079638874b2131c390f4eb2cda4
Krystian Petek <krystianpetek2@gmail.com>
2023-06-22 06:31:17 +0200

    Added angular material components to dashboard view, added styling for dashboard inner navigation bar and for whole content in module

--- src/WebUI/GarageGenius.WebUI/src/app/dashboard/dashboard-routing.module.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/dashboard/dashboard-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { authenticationGuard } from '../shared/guards/authentication.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+const usersModule = () =>
+  import('../users/users.module').then((m) => m.UsersModule);
+const vehiclesModule = () =>
+  import('../vehicles/vehicles.module').then((m) => m.VehiclesModule);
+
 const routes: Routes = [
   {
     path: '',
@@ -10,17 +15,20 @@ const routes: Routes = [
     canActivate: [authenticationGuard],
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'vehicles',
+      },
+      {
         path: 'users',
         pathMatch: 'prefix',
-        loadChildren: () =>
-          import('../users/users.module').then((m) => m.UsersModule),
+        loadChildren: usersModule,
         canActivate: [authenticationGuard],
       },
       {
         path: 'vehicles',
         pathMatch: 'prefix',
-        loadChildren: () =>
-          import('../vehicles/vehicles.module').then((m) => m.VehiclesModule),
+        loadChildren: vehiclesModule,
         canActivate: [authenticationGuard],
       },
     ],
--- src/WebUI/GarageGenius.WebUI/src/app/dashboard/dashboard.module.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { AppMaterialModule } from '../shared/app-material.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  imports: [CommonModule, DashboardRoutingModule, AppMaterialModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
