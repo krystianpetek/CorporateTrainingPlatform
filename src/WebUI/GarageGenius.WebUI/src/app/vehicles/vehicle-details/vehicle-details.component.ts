47421656e648f126e2918d78b6f86ffdd83be0fa
Krystian Petek <krystianpetek2@gmail.com>
2023-06-27 21:02:56 +0200

    feature: added error handling for vehicle details component, created whole view with details about vehicle, added support for back button, fix: fixed main base component positioning in mobile size

--- src/WebUI/GarageGenius.WebUI/src/app/vehicles/vehicle-details/vehicle-details.component.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/vehicles/vehicle-details/vehicle-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { VehiclesService as VehicleService } from '../service/vehicles.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { VehicleResponseModel } from '../models/vehicle.model';
+import { catchError, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-vehicle-details',
@@ -10,28 +12,48 @@ import { VehicleResponseModel } from '../models/vehicle.model';
 })
 export class VehicleDetailsComponent implements OnInit {
   private readonly _vehicleService: VehicleService;
-  private readonly _router: ActivatedRoute;
+  private readonly _activatedRoute: ActivatedRoute;
+  private readonly _router: Router;
   public vehicleResponse?: VehicleResponseModel;
 
-  constructor(vehicleService: VehicleService, router: ActivatedRoute) {
+  constructor(
+    vehicleService: VehicleService,
+    activatedRoute: ActivatedRoute,
+    router: Router
+  ) {
     this._vehicleService = vehicleService;
+    this._activatedRoute = activatedRoute;
     this._router = router;
   }
   ngOnInit(): void {
-    this._router.params.subscribe((params) => {
+    this._activatedRoute.params.subscribe((params) => {
       const vehicleId = params['id'];
       this.vehicle(vehicleId);
     });
   }
 
   public vehicle(id: string) {
-    return this._vehicleService.getVehicleById(id).subscribe((vehicle) => {
-      this.vehicleResponse = vehicle;
-    });
+    return this._vehicleService
+      .getVehicleById(id)
+      .pipe(catchError(this.handleError))
+      .subscribe((vehicle) => {
+        this.vehicleResponse = vehicle;
+      });
   }
 
   public goBack(): void {
-    window.history.back();
-    // TODO - change a back button handling
+    this._router.navigate(['dashboard/vehicles']);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${error.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+    }
+    console.error(errorMessage);
+    // TODO - add a remote logging service like in backend - Serilog with Seq sink
+    return throwError(() => errorMessage);
   }
 }
