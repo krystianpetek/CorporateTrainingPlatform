4489e165c7ba36ad9815f480ec06112d0bce0113
Krystian Petek <krystianpetek2@gmail.com>
2023-06-29 07:31:52 +0200

    feat: created routing for reservations by vehicle id

--- src/WebUI/GarageGenius.WebUI/src/app/reservations/reservations-routing.module.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/reservations/reservations-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authenticationGuard } from '../shared/guards/authentication.guard';
 import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { ReservationDetailsComponent } from './reservation-details/reservation-details.component';
+import { ReservationAddComponent } from './reservation-add/reservation-add.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ReservationListComponent,
+    canActivate: [authenticationGuard],
+  },
+  {
+    path: ':id',
+    component: ReservationDetailsComponent,
+    canActivate: [authenticationGuard],
+  },
+  {
+    path: 'add',
+    component: ReservationAddComponent,
     canActivate: [authenticationGuard],
   },
 ];
