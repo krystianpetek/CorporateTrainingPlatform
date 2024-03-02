a3e8e4eb024ddb5697f92f5113cf662e2b18561c
Krystian Petek <krystianpetek2@gmail.com>
2024-03-02 22:25:39 +0100

    feature: added editMode and updating account information in account component

--- src/WebUI/GarageGenius.WebUI/src/app/account/account.module.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/account/account.module.ts
@@ -6,6 +6,13 @@ import {authenticationGuard} from "../shared/guards/authentication.guard";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatRadioModule} from "@angular/material/radio";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 const routes: Routes = [
   {
@@ -17,13 +24,20 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AccountComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    MatButtonModule,
-    MatIconModule,
-    MatRadioModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        MatButtonModule,
+        MatIconModule,
+        MatRadioModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatOptionModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatTooltipModule
+    ],
   exports: [RouterModule],
 })
 export class AccountModule { }
--- src/WebUI/GarageGenius.WebUI/src/app/account/account/account.component.ts
+++ src/WebUI/GarageGenius.WebUI/src/app/account/account/account.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../users/services/user.service";
 import {CustomersService} from "../../customers/services/customers.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {UpdateCustomerFormModel} from "../models/update-customer-form.model";
 
 @Component({
   selector: 'app-account',
@@ -25,9 +27,26 @@ export class AccountComponent implements OnInit {
     emailAddress: string;
   }
 
+  public roleMap: Map<string, string>  = new Map([
+    ['administrator', 'Administrator'],
+    ['manager', 'Manager'],
+    ['employee', 'Pracownik'],
+    ['customer', 'Klient']
+  ]);
+
+  public stateMap: Map<string, string>  = new Map([
+    ['Active', 'Aktywny'],
+    ['Unactive', 'Nieaktywny']
+    ]);
+
+  public editMode: boolean = false;
+  public updateAccountForm!: FormGroup<UpdateCustomerFormModel>;
+
   constructor(
     private userService: UserService,
-    private customersService: CustomersService) {}
+    private customersService: CustomersService,
+    private _formBuilder: FormBuilder
+    ) {}
 
   ngOnInit() {
 
@@ -45,9 +64,36 @@ export class AccountComponent implements OnInit {
                 phoneNumber : customerResponse?.phoneNumber?.value,
                 emailAddress : customerResponse?.emailAddress?.value
               }
+
+              this.updateAccountForm = this._formBuilder.group({
+                id: [responseUser.customerId],
+                firstName: [customerResponse.firstName?.value],
+                lastName: [customerResponse.lastName?.value],
+                phoneNumber: [customerResponse.phoneNumber?.value],
+              });
+
             })
         }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
+  public editAccount(): void {
+    this.editMode = !this.editMode;
+  }
+
+  public saveAccountChanges(): void {
+    this.customersService.updateCustomer({
+      id : this.updateAccountForm.value.id!,
+      firstName : this.updateAccountForm.value.firstName!,
+      lastName : this.updateAccountForm.value.lastName!,
+      phoneNumber : this.updateAccountForm.value.phoneNumber!,
+    }).subscribe(
+      (response) => {
+        window.location.reload();
       },
       (error) => {
         console.error(error);
