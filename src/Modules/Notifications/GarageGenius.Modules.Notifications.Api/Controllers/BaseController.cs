0062dd5797ad868aee205fd77f2b105b1c0f804c
Krystian Petek <krystianpetek2@gmail.com>
2024-03-17 17:07:28 +0100

    feature: added notifications controller with first action to get logged user notifications

---
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Api/Controllers/BaseController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GarageGenius.Modules.Notifications.Api.Controllers;
+
+[ApiController]
+[Route($"{NotificationsModule.BasePath}/[controller]")]
+public abstract class BaseController : ControllerBase { }
---
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using GarageGenius.Modules.Notifications.Application.Queries;
+using GarageGenius.Modules.Notifications.Application.Queries.GetUserNotifications;
+using GarageGenius.Shared.Abstractions.Dispatcher;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GarageGenius.Modules.Notifications.Api.Controllers;
+
+public class NotificationsController : BaseController
+{
+	private readonly IDispatcher _dispatcher;
+
+	public NotificationsController(IDispatcher dispatcher)
+	{
+		_dispatcher = dispatcher;
+	}
+
+	[Authorize]
+	[HttpGet("notifications")]
+	[SwaggerOperation("Get logged user notifications")]
+	[SwaggerResponse(StatusCodes.Status200OK, "User notifications found", typeof(GetUserNotificationsQuery))]
+	public async Task<ActionResult> GetNotificationsForUserAsync(CancellationToken cancellationToken)
+	{
+		var identifier = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+		var userId = Guid.Parse(identifier);
+		var notifications = await _dispatcher.DispatchQueryAsync(new GetUserNotificationsQuery(userId), cancellationToken);
+
+		return Ok(notifications);
+	}
+}
+// TODO - handling for timeout / cancel request
