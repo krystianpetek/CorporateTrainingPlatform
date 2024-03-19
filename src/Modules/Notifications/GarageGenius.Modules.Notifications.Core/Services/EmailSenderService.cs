06694bc9b2356f618f2bd46a9a84332e1a30ae68
Krystian Petek <krystianpetek2@gmail.com>
2024-03-19 21:08:15 +0100

    feature: added implementation for sending email using gmail

--- src/Modules/Notifications/GarageGenius.Modules.Notifications.Core/Services/EmailSenderService.cs
+++ src/Modules/Notifications/GarageGenius.Modules.Notifications.Core/Services/EmailSenderService.cs
@@ -1,4 +1,7 @@
-﻿namespace GarageGenius.Modules.Notifications.Core.Services;
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace GarageGenius.Modules.Notifications.Core.Services;
 internal class EmailSenderService : IEmailSenderService
 {
 	private readonly Serilog.ILogger _logger;
@@ -11,7 +14,25 @@ internal class EmailSenderService : IEmailSenderService
 
 	public Task SendEmailAsync(string receiver, string subject, string message)
 	{
-		// TODO: create sending email
+        const string fromMail = "sa.garagegenius@gmail.com";
+        const string fromAppPassword = "";
+
+        var mail = new MailMessage();
+        mail.From = new MailAddress(fromMail);
+        mail.Subject = subject;
+        mail.Body = $"<html><body>{message}<body><html>";
+        mail.IsBodyHtml = true;
+        mail.To.Add(new MailAddress(receiver));
+
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+	        Port = 587,
+	        Credentials = new NetworkCredential(fromMail, fromAppPassword),
+	        EnableSsl = true
+        };
+
+        smtpClient.Send(mail);
+
 		_logger.Information(
 			messageTemplate: "Email sent to {Receiver} with subject {Subject} and message {Message}",
 			receiver,
